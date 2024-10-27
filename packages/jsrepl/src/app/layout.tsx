4a4bd59ae1c5fef107fde63b7a95536e6f4f466d
Aleksei Nagovitsyn <al3x.nag@gmail.com>
2024-10-27 21:40:18 +0500

    Disable umami for test env

--- packages/jsrepl/src/app/layout.tsx
+++ packages/jsrepl/src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <body>
         <TooltipProvider>{children}</TooltipProvider>
         <Toaster />
-        {process.env.NODE_ENV === 'production' && (
+        {process.env.NEXT_PUBLIC_NODE_ENV === 'production' && (
           <Script
             src="https://cloud.umami.is/script.js"
             data-website-id="23c57346-41a9-4b0f-a07a-76f8bf7c4ff3"
